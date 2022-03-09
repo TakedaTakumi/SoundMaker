7c98752726d8e9a5923bc572673ebb611f3975c5
Takeda Takumi <t.takeda@guncys.com>
2022-03-09 12:18:16 +0900

    feat: update make sound file

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,5 @@
-import fs from 'fs';
-import { RenderingAudioContext as AudioContext } from '@descript/web-audio-js';
-const context = new AudioContext();
+import { makeScaleFile } from './makeSound';
 
-const HzBase = Math.pow(2, 1 / 12);
-const getHz = (semitone: number, octave = 4): number => {
-	const _o = (octave - 4) * 12;
-	const _s = semitone - 9;
-	return 440 * Math.pow(HzBase, _s + _o);
-};
-const doremi = () => {
-	const t0 = context.currentTime;
-	let t = 0;
-
-	const oscillator = context.createOscillator();
-	const gain = context.createGain();
-
-	oscillator.type = 'square'; //sine, square, sawtooth, triangle, custom
-	[0, 2, 4, 5, 7, 9, 11, 12].forEach((s) => {
-		const hz = getHz(s);
-		oscillator.frequency.setValueAtTime(hz, t0 + t);
-		gain.gain.setValueAtTime(0.01, t0 + t);
-		t += 1;
-	});
-	oscillator.start(t0);
-	oscillator.stop(t0 + t);
-	oscillator.connect(gain, 0, 0);
-
-	gain.connect(context.destination, 0, 0);
-
-	context.processTo(t0 + t);
-};
-
-doremi();
-
-const audioData = context.exportAsAudioData();
-
-context.encodeAudioData(audioData, {}).then((arrayBuffer) => {
-	fs.writeFile('output.wav', Buffer.from(arrayBuffer), (err) => {
-		if (err) throw err;
-		console.log('出力しました。');
-	});
-});
+// makeDoremi();
+// makeKiraKira();
+makeScaleFile();
---
+++ src/makeSound.ts
@@ -0,0 +1,165 @@
+import fs from 'fs';
+import { RenderingAudioContext as AudioContext } from '@descript/web-audio-js';
+
+//     const base = 440; //ラ=440(442)Hz
+//     const d = Math.pow(2, 1 / 12);
+//     console.log(base * Math.pow(d, -9)); //ド    C maj
+//     console.log(base * Math.pow(d, -8)); //ド#   D♭ maj
+//     console.log(base * Math.pow(d, -7)); //レ    D maj
+//     console.log(base * Math.pow(d, -6)); //レ#   E♭ maj
+//     console.log(base * Math.pow(d, -5)); //ミ    E maj
+//     console.log(base * Math.pow(d, -4)); //ファ  F maj
+//     console.log(base * Math.pow(d, -3)); //ファ# G♭ maj
+//     console.log(base * Math.pow(d, -2)); //ソ    G maj
+//     console.log(base * Math.pow(d, -1)); //ソ#   A♭ min
+//     console.log(base * Math.pow(d, 0)); // ラ    A min
+//     console.log(base * Math.pow(d, 1)); // ラ#   B♭ min
+//     console.log(base * Math.pow(d, 2)); // シ    B min
+//     console.log(base * Math.pow(d, 3)); // ド    C min
+
+enum KEY_SCALE {
+	C_MAJOR = 0,
+	D_FLAT_MAJOR,
+	D_MAJOR,
+	E_FLAT_MAJOR,
+	E_MAJOR,
+	F_MAJOR,
+	G_FLAT_MAJOR,
+	G_MAJOR,
+	A_FLAT_MINOR,
+	A_MINOR,
+	B_FLAT_MINOR,
+	B_MINOR,
+	C_MINOR,
+}
+
+const HzBase = Math.pow(2, 1 / 12);
+function getHz(semitone: number, octave = 4): number {
+	const _o = (octave - 4) * 12;
+	const _s = semitone - 9;
+	return 440 * Math.pow(HzBase, _s + _o);
+}
+
+class HzCode {
+	readonly hz: number;
+	readonly div: number;
+	readonly long: number;
+
+	static make(scale: KEY_SCALE, div: number, long: number): HzCode {
+		return new HzCode(scale, div, long);
+	}
+
+	private constructor(hzCode: number, div: number, long: number) {
+		this.hz = getHz(hzCode);
+		this.div = div;
+		this.long = long;
+	}
+}
+
+function saveFile(ctx: AudioContext, outFileName: string) {
+	const audioData = ctx.exportAsAudioData();
+
+	ctx.encodeAudioData(audioData, {}).then((arrayBuffer) => {
+		const dir = 'dest';
+
+		if (fs.existsSync(dir) === false) {
+			fs.mkdirSync(dir);
+		}
+
+		const path = `${dir}/${outFileName}.wav`;
+		fs.writeFileSync(path, Buffer.from(arrayBuffer));
+
+		console.log(path, '出力しました。');
+	});
+}
+
+export function makeDoremi(outFileName = 'doremi') {
+	const score = [
+		HzCode.make(KEY_SCALE.C_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.D_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.E_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.F_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.G_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.A_MINOR, 4, 1),
+		HzCode.make(KEY_SCALE.B_MINOR, 4, 1),
+		HzCode.make(KEY_SCALE.C_MINOR, 4, 1),
+	];
+	const context = new AudioContext();
+
+	playScore(context, score);
+
+	saveFile(context, outFileName);
+}
+
+export function playScore(ctx: AudioContext, score: Array<HzCode>) {
+	const t0 = ctx.currentTime;
+	let t = 0;
+
+	const oscillator = ctx.createOscillator();
+	const gain = ctx.createGain();
+
+	oscillator.type = 'square'; //sine, square, sawtooth, triangle, custom
+	score.forEach((s) => {
+		const hz = s.hz;
+		const d = (60 / 80) * (4 / s.div) * s.long; //テンポ80
+		oscillator.frequency.setValueAtTime(hz, t0 + t);
+		gain.gain.setValueCurveAtTime([0.01, 0.02, 0.02, 0.005], t0 + t, d);
+		t += d;
+	});
+	oscillator.start(t0);
+	oscillator.stop(t0 + t);
+	oscillator.connect(gain, 0, 0);
+
+	gain.connect(ctx.destination, 0, 0);
+
+	ctx.processTo(t0 + t);
+
+	return;
+}
+
+export function makeKiraKira(outFileName = 'kirakira') {
+	const score = [
+		HzCode.make(KEY_SCALE.C_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.C_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.G_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.G_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.A_MINOR, 4, 1),
+		HzCode.make(KEY_SCALE.A_MINOR, 4, 1),
+		HzCode.make(KEY_SCALE.G_MAJOR, 4, 2),
+		HzCode.make(KEY_SCALE.F_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.F_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.E_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.E_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.D_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.D_MAJOR, 4, 1),
+		HzCode.make(KEY_SCALE.C_MAJOR, 4, 2),
+	];
+
+	const audioContext = new AudioContext();
+
+	playScore(audioContext, score);
+
+	saveFile(audioContext, outFileName);
+}
+
+function playToSave(score: HzCode[], outFileName: string) {
+	const audioContext = new AudioContext();
+	playScore(audioContext, score);
+	saveFile(audioContext, outFileName);
+}
+
+export function makeScaleFile() {
+	playToSave([HzCode.make(KEY_SCALE.C_MAJOR, 4, 4)], 'C_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.D_FLAT_MAJOR, 4, 4)], 'D_FLAT_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.D_MAJOR, 4, 4)], 'D_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.E_FLAT_MAJOR, 4, 4)], 'E_FLAT_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.E_MAJOR, 4, 4)], 'E_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.F_MAJOR, 4, 4)], 'F_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.G_FLAT_MAJOR, 4, 4)], 'G_FLAT_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.G_MAJOR, 4, 4)], 'G_MAJOR');
+	playToSave([HzCode.make(KEY_SCALE.A_FLAT_MINOR, 4, 4)], 'A_FLAT_MINOR');
+	playToSave([HzCode.make(KEY_SCALE.A_MINOR, 4, 4)], 'A_MINOR');
+	playToSave([HzCode.make(KEY_SCALE.B_FLAT_MINOR, 4, 4)], 'B_FLAT_MINOR');
+	playToSave([HzCode.make(KEY_SCALE.B_MINOR, 4, 4)], 'B_MINOR');
+	playToSave([HzCode.make(KEY_SCALE.C_MINOR, 4, 4)], 'C_MINOR');
+}
